43c7d66889d71bab4501197e6052ed75c6dc743d
anas tellat <anas tellat>
2017-08-09 11:20:08 -0400

    recommiting all files

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsArray = [
+	{
+		name: 'Nounous',
+		photo: 'http://i.imgur.com/RRUe0Mo.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Moe',
+		photo: 'https://www.codeproject.com/KB/GDI-plus/ImageProcessing2/flip.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lana',
+		photo: 'http://media.routard.com/image/52/6/photo.1407526.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Maya',
+		photo: 'http://res.cloudinary.com/7adf6ngw/image/upload/sample.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jasmin',
+		photo: 'https://www.reduceimages.com/img/image-after.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
